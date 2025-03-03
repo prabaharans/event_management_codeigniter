6f9ca83b93b174e48669f10dce927ea9ef18ba26
Prabaharan <spraba84@gmail.com>
2025-03-03 11:20:27 +0530

    validation in progress

--- app/Views/includes/event_form.php
+++ app/Views/includes/event_form.php
@@ -1,11 +1,11 @@
-<link href="<?= base_url('spry/textfieldvalidation/SpryValidationTextField.css') ?>" rel="stylesheet" type="text/css" />
-<script src="<?= base_url('spry/textfieldvalidation/SpryValidationTextField.js') ?>" type="text/javascript"></script>
 
-<link href="<?= base_url('spry/textareavalidation/SpryValidationTextarea.css') ?>" rel="stylesheet" type="text/css" />
-<script src="<?= base_url('spry/textareavalidation/SpryValidationTextarea.js') ?>" type="text/javascript"></script>
 
-<link href="<?= base_url('spry/selectvalidation/SpryValidationSelect.css') ?>" rel="stylesheet" type="text/css" />
-<script src="<?= base_url('spry/selectvalidation/SpryValidationSelect.js') ?>" type="text/javascript"></script>
+<?= $this->section('pageStyles') ?>
+	<link href="<?= base_url('spry/textfieldvalidation/SpryValidationTextField.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url('spry/textareavalidation/SpryValidationTextarea.css') ?>" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url('spry/selectvalidation/SpryValidationSelect.css') ?>" rel="stylesheet" type="text/css" />
+<?= $this->endSection() ?>
+
 
 <div class="card card-info">
   <div class="card-header">
@@ -13,13 +13,13 @@
   </div>
   <!-- /.box-header -->
   <!-- form start -->
-  <form role="form" action="<?= base_url('api/process.php?cmd=book') ?>" method="post">
+  <form role="form" action="<?= base_url('api/process.php?cmd=book') ?>" name="frmBookEvent" id="frmBookEvent" method="post">
     <div class="card-body p-2">
       <div class="form-group">
         <label for="exampleInputEmail1" class="text-small">Name</label>
 		<input type="hidden" name="userId" value=""  id="userId"/>
-        <span id="sprytf_name">
-		<select name="name" class="form-control form-control-sm input-sm">
+        <span id="tf_name">
+		<select name="name" id="name" class="form-control form-control-sm input-sm">
 			<option>--select user--</option>
 			<?php
 			// $sql = "SELECT id, name FROM tbl_users";
@@ -39,23 +39,23 @@
 
 	  <div class="form-group">
         <label for="exampleInputEmail1" class="text-small">Address</label>
-		<span id="sprytf_address">
+		<span id="tf_address">
         <textarea name="address" class="form-control form-control-sm input-sm" placeholder="Address" id="address"></textarea>
 		<span class="textareaRequiredMsg text-small">Address is required.</span>
 		<span class="textareaMinCharsMsg text-small">Address must specify at least 10 characters.</span>
 		</span>
       </div>
 	  <div class="form-group">
-        <label for="exampleInputEmail1" class="text-small">Phone</label>
-		<span id="sprytf_phone">
-        <input type="text" name="phone" class="form-control form-control-sm input-sm"  placeholder="Phone number" id="phone">
-		<span class="textfieldRequiredMsg text-small">Phone number is required.</span>
+        <label for="exampleInputEmail1" class="text-small">Mobile</label>
+		<span id="tf_mobile">
+        <input type="mobile" name="mobile" class="form-control form-control-sm input-sm"  placeholder="Mobile number" id="mobile">
+		<span class="textfieldRequiredMsg text-small">Mobile number is required.</span>
 		</span>
       </div>
 	  <div class="form-group">
         <label for="exampleInputEmail1" class="text-small">Email address</label>
-		<span id="sprytf_email">
-        <input type="text" name="email" class="form-control form-control-sm input-sm" placeholder="Enter email" id="email">
+		<span id="tf_email">
+        <input type="email" name="email" class="form-control form-control-sm input-sm" placeholder="Enter email" id="email" data-inputmask-alias="email">
 		<span class="textfieldRequiredMsg text-small">Email ID is required.</span>
 		<span class="textfieldInvalidFormatMsg text-small">Please enter a valid email (user@domain.com).</span>
 		</span>
@@ -65,16 +65,16 @@
       <div class="row">
       	<div class="col-6">
 			<label class="text-small">Reservation Date</label>
-			<span id="sprytf_rdate">
-        	<input type="text" name="rdate" class="form-control form-control-sm" placeholder="YYYY-mm-dd">
+			<span id="tf_rdate">
+        	<input type="date" name="rdate" class="form-control form-control-sm" placeholder="YYYY-mm-dd">
 			<span class="textfieldRequiredMsg text-small">Date is required.</span>
 			<span class="textfieldInvalidFormatMsg text-small">Invalid date Format.</span>
 			</span>
         </div>
         <div class="col-6">
 			<label class="text-small">Reservation Time</label>
-			<span id="sprytf_rtime">
-            <input type="text" name="rtime" class="form-control form-control-sm" placeholder="HH:mm">
+			<span id="tf_rtime">
+            <input type="time" name="rtime" class="form-control form-control-sm" placeholder="HH:mm">
 			<span class="textfieldRequiredMsg text-small">Time is required.</span>
 			<span class="textfieldInvalidFormatMsg text-small">Invalid time Format.</span>
 			</span>
@@ -84,8 +84,8 @@
 
 	  <div class="form-group">
         <label for="exampleInputPassword1" class="text-small">No of Peoples</label>
-		<span id="sprytf_ucount">
-        <input type="text" name="ucount" class="form-control form-control-sm input-sm" placeholder="No of peoples" >
+		<span id="tf_ucount">
+        <input type="number" name="ucount" min="1" max="100" class="form-control form-control-sm input-sm" placeholder="No of peoples" >
 		<span class="textfieldRequiredMsg text-small">No of peoples is required.</span>
 		<span class="textfieldInvalidFormatMsg text-small">Invalid Format.</span>
       </div>
@@ -96,26 +96,180 @@
   </form>
 </div>
 <!-- /.box -->
-<script type="text/javascript">
 
-var sprytf_name 	= new Spry.Widget.ValidationSelect("sprytf_name");
-var sprytf_address 	= new Spry.Widget.ValidationTextarea("sprytf_address", {minChars:6, isRequired:true, validateOn:["blur", "change"]});
-var sprytf_phone 	= new Spry.Widget.ValidationTextField("sprytf_phone", 'none', {validateOn:["blur", "change"]});
-var sprytf_mail 	= new Spry.Widget.ValidationTextField("sprytf_email", 'email', {validateOn:["blur", "change"]});
-var sprytf_rdate 	= new Spry.Widget.ValidationTextField("sprytf_rdate", "date", {format:"yyyy-mm-dd", useCharacterMasking: true, validateOn:["blur", "change"]});
-var sprytf_rtime 	= new Spry.Widget.ValidationTextField("sprytf_rtime", "time", {hint:"i.e 20:10", useCharacterMasking: true, validateOn:["blur", "change"]});
-var sprytf_ucount 	= new Spry.Widget.ValidationTextField("sprytf_ucount", "integer", {validateOn:["blur", "change"]});
+<?= $this->section('pageScripts') ?>
+	<script src="<?= base_url('jquery.validate/jquery.validate.min.js') ?>" type="text/javascript"></script>
+	<script src="<?= base_url('jquery.validate/additional-methods.min.js') ?>" type="text/javascript"></script>
+	<script src="<?= base_url('jquery.inputmask/jquery.inputmask.min.js') ?>" type="text/javascript"></script>
+	<!-- <script src="<?= base_url('jquery.mask/jquery.mask.min.js') ?>" type="text/javascript"></script> -->
+	<!-- <script src="<?= base_url('spry/textfieldvalidation/SpryValidationTextField.js') ?>" type="text/javascript"></script>
+	<script src="<?= base_url('spry/textareavalidation/SpryValidationTextarea.js') ?>" type="text/javascript"></script>
+	<script src="<?= base_url('spry/selectvalidation/SpryValidationSelect.js') ?>" type="text/javascript"></script> -->
 
-$('select').on('change', function() {
-	//alert( this.value );
-	var id = this.value;
-	$.get('<?= base_url('api/process.php?cmd=user&userId=') ?>'+id, function(data, status){
-		var obj = $.parseJSON(data);
-		$('#userId').val(obj.user_id);
-		$('#email').val(obj.email);
-		$('#address').val(obj.address);
-		$('#phone').val(obj.phone_no);
-	});
+	<script type="text/javascript">
+		var langEventFormJSON = <?php echo $line = json_encode(lang('EventManagement.validation.messages.eventForm', [], 'english'));
+; ?>;
+		if(langEventFormJSON != '') {
+			// var langEventFormArr = JSON.parse(langEventFormJSON);
+			// console.log('langEventFormJSON');
+			// console.log(langEventFormJSON);
+			// console.log(langEventFormJSON.eventName.required);
+		}
+	// var sprytf_name 	= new Spry.Widget.ValidationSelect("sprytf_name");
+	// var sprytf_address 	= new Spry.Widget.ValidationTextarea("sprytf_address", {minChars:6, isRequired:true, validateOn:["blur", "change"]});
+	// var sprytf_phone 	= new Spry.Widget.ValidationTextField("sprytf_phone", 'none', {validateOn:["blur", "change"]});
+	// var sprytf_mail 	= new Spry.Widget.ValidationTextField("sprytf_email", 'email', {validateOn:["blur", "change"]});
+	// var sprytf_rdate 	= new Spry.Widget.ValidationTextField("sprytf_rdate", "date", {format:"yyyy-mm-dd", useCharacterMasking: true, validateOn:["blur", "change"]});
+	// var sprytf_rtime 	= new Spry.Widget.ValidationTextField("sprytf_rtime", "time", {hint:"i.e 20:10", useCharacterMasking: true, validateOn:["blur", "change"]});
+	// var sprytf_ucount 	= new Spry.Widget.ValidationTextField("sprytf_ucount", "integer", {validateOn:["blur", "change"]});
 
-})
-</script>
+	// $('select').on('change', function() {
+	// 	//alert( this.value );
+	// 	var id = this.value;
+	// 	$.get('<?= base_url('api/process.php?cmd=user&userId=') ?>'+id, function(data, status){
+	// 		var obj = $.parseJSON(data);
+	// 		$('#userId').val(obj.user_id);
+	// 		$('#email').val(obj.email);
+	// 		$('#address').val(obj.address);
+	// 		$('#phone').val(obj.phone_no);
+	// 	});
+
+	// })
+
+	$.validator.addMethod("regxEmail", function(value, element) {
+		return this.optional(element) || /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(value);
+	}, langEventFormJSON.eventEmail.regxEmail);
+
+
+   if ($("#frmBookEvent").length > 0) {
+		// $("#mobile").inputmask("(99)-9999-9999");
+		// $("#mobile").inputmask("+99-9999999999");
+		// $("#mobile").inputmask("(999)-999-9999");
+		// $("#mobile").inputmask("+999-99-99-999999");
+		// $("#mobile").inputmask("09999999999");
+		// $("#mobile").inputmask("9999999999");
+
+		// $("#mobile").mask("+99-9999999999", {placeholder:"+__-________"});
+		$("#mobile").inputmask("+99-9999999999");
+		// $("#email").mask("abc@def.ghi", {placeholder:"___@____.___"});
+		// $("#email").inputmask({ alias: "email"});
+		// $("#email").inputmask("abc@def.ghi");
+		// Inputmask( "/[a-zA-Z0-9_]/@/[a-zA-Z0-9]/./[a-zA-Z]{2,4}/").mask("#email");
+
+		// $('#email').mask('', {
+		// 	// translation: {
+		// 	// 	'A': { pattern: /[a-zA-Z0-9_]/ },
+		// 	// 	'B': { pattern: /[a-zA-Z0-9]/ },
+		// 	// 	'C': { pattern: /[a-zA-Z]{2,4}/ }
+		// 	// },
+		// 	clearIfNotMatch: true, // Clear the input if it doesn't match the pattern
+		// 	placeholder:"___@____.___"
+		// });
+		// $('#rdate').mask('1111-11-11');
+		// $('#rdate').inputmask('YYYY-MM-DD');
+		// $('#rtime').mask('00:00:00');
+      	$("#frmBookEvent").validate({
+			// focusCleanup: true,
+			errorClass: "invalid-feedback",
+			errorElement: "span",
+			rules: {
+				name: {
+					required: true,
+				},
+				address: {
+					required: true,
+					minlength:10,
+				},
+				mobile: {
+					required: true,
+					minlength:10,
+					maxlength:17,
+				},
+				email: {
+					required: true,
+					maxlength: 50,
+					email: true,
+					regxEmail: true
+				},
+				rdate: {
+					required: true,
+				},
+				rtime: {
+					required: true,
+				},
+				ucount: {
+					required: true,
+				},
+			},
+			messages: {
+				name: {
+					required: langEventFormJSON.eventName.required,
+				},
+				address: {
+					required: langEventFormJSON.eventAddress.required,
+				},
+				email: {
+					required: langEventFormJSON.eventEmail.required,
+					email: langEventFormJSON.eventEmail.isValid,
+					maxlength: langEventFormJSON.eventEmail.maxlength,
+					regxEmail: langEventFormJSON.eventEmail.regxEmail,
+				},
+				mobile: {
+					required: langEventFormJSON.eventMobile.required,
+					number: langEventFormJSON.eventMobile.number,
+					minlength: langEventFormJSON.eventMobile.minlength,
+					maxlength: langEventFormJSON.eventMobile.maxlength,
+				},
+				rdate: {
+					required: langEventFormJSON.eventReservationDate.required,
+				},
+				rtime: {
+					required: langEventFormJSON.eventReservationTime.required,
+				},
+
+				ucount: {
+					required: langEventFormJSON.eventNoOfPeople.required,
+				},
+			},
+			highlight: function (element) {
+				$(element).closest('.form-control').removeClass('is-valid').addClass('is-invalid');
+				// $(element).closest('label').removeClass('invalid-feedback').addClass('valid-feedback');
+				$( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
+			},
+			unhighlight: function ( element, errorClass, validClass ) {
+				$( element ).closest('.form-control').removeClass('is-invalid').addClass('is-valid');
+				// $(element).closest('label').removeClass('invalid-feedback').addClass('valid-feedback');
+				$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
+			},
+			success: function (element) {
+				$( element ).closest('.form-control').removeClass('is-invalid').addClass('is-valid');
+				// $(element).closest('label').removeClass('invalid-feedback').addClass('valid-feedback');
+				$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
+			}
+			// submitHandler: function(form) {
+			// 	$('#btnadd').val('Sending..');
+			// 	$.ajax({
+			// 		url: "<?php echo site_url('student/store') ?>",
+			// 		type: "POST",
+			// 		data: $('#frmAddStudent').serialize(),
+			// 		dataType: "json",
+			// 		success: function(response) {
+			// 			console.log(response);
+			// 			console.log(response.success);
+			// 			$('#btnadd').val('Add');
+			// 			$('#res_message').html(response.msg);
+			// 			$('#res_message').show();
+			// 			$('#res_message').removeClass('d-none');
+			// 			$('#frmAddStudent')[0].reset();
+			// 			setTimeout(function() {
+			// 				$('#res_message').hide();
+			// 				$('#res_message').html('');
+			// 			}, 3000);
+			// 		}
+			// 	});
+			// }
+		})
+	}
+	</script>
+
+<?= $this->endSection() ?>
