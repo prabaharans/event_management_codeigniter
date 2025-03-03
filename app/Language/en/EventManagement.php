60d68b7308a363410339512ad83fe506d4ff3702
Prabaharan <spraba84@gmail.com>
2025-03-03 14:25:45 +0530

    validation message fixed

--- app/Language/en/EventManagement.php
+++ app/Language/en/EventManagement.php
@@ -6,16 +6,16 @@ return [
         'messages' => [
             'eventForm' => [
                 'eventName' => [
-                    'required' => 'Please enter the name'
+                    'required' => 'Please select the name'
                 ],
                 'eventAddress' => [
                     'required' => 'Please enter the address'
                 ],
                 'eventEmail' => [
                     'required' => 'Please enter the email',
-                    'isValid' => 'Please enter valid email',
-                    'maxlength' => "The email name should less than or equal to ##REQUIREREPLACE## characters",
-                    'regxEmail' => "Email Address is invalid: Please enter a valid email address.",
+                    'isValid' => 'Please enter valid email (user@domain.com)',
+                    'maxlength' => "The email should less than or equal to ##REQUIREREPLACE## characters",
+                    'regxEmail' => "Email Address is invalid: Please enter a valid email address. (user@domain.com)",
                 ],
                 'eventMobile' => [
                     'required' => 'Please enter the mobile number',
@@ -31,6 +31,7 @@ return [
                 ],
                 'eventNoOfPeople' => [
                     'required' => 'Please enter the no of people',
+                    'maxlength' => "The no of people should less than or equal to ##REQUIREREPLACE## digits",
                 ]
             ]
         ]
