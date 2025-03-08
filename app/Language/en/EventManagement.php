61061468cf8b433b0858f0efc9d42d5f81f1e5ef
Prabaharan <spraba84@gmail.com>
2025-03-09 02:40:00 +0530

    language file updated

--- app/Language/en/EventManagement.php
+++ app/Language/en/EventManagement.php
@@ -2,6 +2,27 @@
 
 return [
     'dashboard' => 'Dashboard',
+    'book_event' => 'Book Event',
+    'name' => 'Name',
+    'address' => 'Address',
+    'mobile' => 'Mobile',
+    'select_user' => '--select user--',
+    'phone_codes' => 'Phone Codes',
+    'mobile_number' => 'Mobile number',
+    'email_address' => 'Email address',
+    'reservation_date' => 'Reservation Date',
+    'reservation_date_placeholder' => 'YYYY-mm-dd',
+    'reservation_time' => 'Reservation Time',
+    'reservation_time_placeholder' => 'HH:mm',
+    'no_of_peoples' => 'No of Peoples',
+    'submit' => 'Submit',
+    'holidays' => 'Holidays',
+    'holiday_list' => 'Holiday List',
+    'holiday_form' => 'Holiday',
+    'holiday_date' => 'Date',
+    'holiday_date_placeholder' => 'yyyy-mm-dd',
+    'holiday_reason' => 'Reason',
+    'add_holiday' => 'Add Holiday',
     'userInfo' => 'User Information',
     'validation' => [
         'messages' => [
@@ -28,15 +49,23 @@ return [
                     'maxlength' => 'The mobile number should less than or equal to ##REQUIREREPLACE## characters',
                 ],
                 'eventReservationDate' => [
-                    'required' => 'Please enter the reservation date',
+                    'required' => 'Please select the reservation date',
                 ],
                 'eventReservationTime' => [
-                    'required' => 'Please enter the reservation time',
+                    'required' => 'Please select the reservation time',
                 ],
                 'eventNoOfPeople' => [
                     'required' => 'Please enter the no of people',
                     'maxlength' => "The no of people should less than or equal to ##REQUIREREPLACE## digits",
                 ]
+            ],
+            'holidayForm' => [
+                'holidayDate' => [
+                    'required' => 'Please select the date',
+                ],
+                'holidayReason' => [
+                    'required' => 'Please enter the reason',
+                ],
             ]
         ]
     ],
