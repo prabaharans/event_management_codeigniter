f21af193430f6311d947c8b14842472e1a31c985
Prabaharan <spraba84@gmail.com>
2025-03-10 02:27:30 +0530

    validation issue fixed in shield

--- app/Config/Auth.php
+++ app/Config/Auth.php
@@ -26,6 +26,7 @@ use CodeIgniter\Shield\Authentication\Passwords\NothingPersonalValidator;
 use CodeIgniter\Shield\Authentication\Passwords\PwnedValidator;
 use CodeIgniter\Shield\Authentication\Passwords\ValidatorInterface;
 use CodeIgniter\Shield\Models\UserModel;
+use App\Models\UsersModel;
 
 class Auth extends ShieldAuth
 {
@@ -438,7 +439,7 @@ class Auth extends ShieldAuth
      *
      * @var class-string<UserModel>
      */
-    public string $userProvider = UserModel::class;
+    public string $userProvider = UsersModel::class;
 
     /**
      * Returns the URL that a user should be redirected
